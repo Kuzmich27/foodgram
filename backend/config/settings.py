9518ef57611940b414a3579a9fd246d2d455ddb2
Kuzmich <7807426@mail.ru>
2025-09-19 11:15:57 +0000

    Описание изменений с сервера

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -192,9 +192,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR.parent / 'frontend/build/static',  # текущая структура
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 TEMPLATES[0]['DIRS'] = [
     BASE_DIR / 'templates',  # только шаблоны
